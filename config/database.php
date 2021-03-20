920e2bb58c6b691b39b6e1d7c94d6c61c32fda70
Jonel Anderson <jonel.sacramento@unas.edu.pe>
2021-03-19 22:54:52 -0500

    Configurando database

--- config/database.php
+++ config/database.php
@@ -1,4 +1,5 @@
 <?php
+$DATABASE_URL =parse_url('postgres://lfqhtfzphcvjnk:ef2a585044b5531bd82edb318477b1e824bf124d81abe61090f47e6392809c63@ec2-52-7-115-250.compute-1.amazonaws.com:5432/d11kpenrmsgv0h');
 
 return [
 
@@ -13,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,13 +57,14 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL('host'),
+            'port' => $DATABASE_URL('port'),
+            'database' => ltrim($DATABASE_URL["path"],"/"),
+            'username' => $DATABASE_URL('user'),
+            'password' => $DATABASE_URL('pass'),
             'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes' => true,
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
